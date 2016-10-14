c0346379f6d996e1aeda701865b084ace1bff49f
tchapados <thomas.chapados@advantys.com>
2016-10-14 10:38:52 -0400

    initial

---
+++ WebApplication12/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication12.Startup))]
+namespace WebApplication12
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
